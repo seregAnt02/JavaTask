b97e5b19909e0506784cfd8c8a285538651d1f7d
s_antonov02 <s_antonov02@rambler.ru>
2024-06-24 16:36:04 +0500

    add realization aspect

--- spring-boot-starter-demo/spring-aop-service/src/main/java/ru/gb/HttpTimerAutoConfiguration.java
+++ spring-boot-starter-demo/spring-aop-service/src/main/java/ru/gb/HttpTimerAutoConfiguration.java
@@ -1,15 +1,26 @@
 package ru.gb;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.gb.aspect.TimerAspect;
 import ru.gb.http.aop.AopFilter;
+import ru.gb.timer.MyServiceTime;
 
 @Configuration
 //@EnableConfigurationProperties()
 class HttpTimerAutoConfiguration {
     @Bean
-    public AopFilter timerAspect() {
+    public AopFilter timerAopAspect() {
         return new AopFilter();
+    }
+
+    @Bean
+    public MyServiceTime serviceTime(){
+        return new MyServiceTime();
+    }
+
+    @Bean
+    public TimerAspect timerAspect(){
+        return new TimerAspect();
     }
 }
--- spring-boot-starter-demo/spring-aop-service/src/main/java/ru/gb/http/aop/AopFilter.java
+++ spring-boot-starter-demo/spring-aop-service/src/main/java/ru/gb/http/aop/AopFilter.java
@@ -4,13 +4,22 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import ru.gb.timer.MyServiceTime;
 
 import java.io.IOException;
 
 @Slf4j
 @Component
 public class AopFilter implements Filter {
+
+    @Autowired
+    private MyServiceTime myServiceTime;
+
+
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
@@ -21,5 +30,8 @@ public class AopFilter implements Filter {
 
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         log.info("response {} {}", httpServletRequest.getServletPath(), httpServletResponse.getStatus());
+
+        myServiceTime.method1("ok", 1);
+        myServiceTime.method2();
     }
 }
